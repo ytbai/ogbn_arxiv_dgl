335b2838d8eb44d11b90379ee56137dd114cec15
Yuntao Bai <yuntao.bai.1991@gmail.com>
2021-02-26 15:18:33 -0800

    For model evaluation

---
+++ test/test.py
@@ -0,0 +1,20 @@
+import torch
+from ogb.nodeproppred import Evaluator
+
+def test(model_factory, data_factory, dataset_name, split_name, loss_name, metric):
+  evaluator = Evaluator(dataset_name)
+  model = model_factory.model
+
+  model.eval()
+  graph = data_factory.graph
+  y_true = data_factory.labels
+
+  pred_idx, x_masked = data_factory.make_mask(split_name)
+
+  logit_pred = model(graph, x_masked)
+  y_pred = torch.argmax(logit_pred, dim=1, keepdim=True)
+  
+  score = evaluator.eval({"y_true":y_true[pred_idx], "y_pred":y_pred[pred_idx]})[metric]
+  model_factory.append_loss(loss_name, score)
+
+  return score
