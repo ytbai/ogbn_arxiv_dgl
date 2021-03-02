a61a964dbfdbdaf5d376c8d4415a1582c68a0926
Yuntao Bai <yuntao.bai.1991@gmail.com>
2021-03-01 18:06:14 -0800

    Implement linear model

---
+++ model_factory/linear.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+class LinearModel(nn.Module):
+  def __init__(
+    self,
+    d_input,
+    d_output,
+  ):
+  
+    super().__init__()
+    self.d_input = d_input
+    self.d_output = d_output
+
+    self.linear = nn.Linear(d_input, d_output)
+
+  def forward(self, graph, input):
+    return self.linear(input)
